9bff6581d81ee8daae0897e17fe0c57d29e1862a
0xAlvi <alviaptianpr96@gmail.com>
2025-04-21 07:54:39 +0700

    Initial commit

---
+++ api/zora.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+    const { address } = req.query;
+  
+    const query = `
+      query {
+        zoraTokenAllocation(identifierWalletAddresses: "${address}") {
+          totalTokensEarned {
+            totalTokens
+          }
+        }
+      }
+    `;
+  
+    try {
+      const response = await fetch("https://api.zora.co/universal/graphql", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query }),
+      });
+      const data = await response.json();
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.status(200).json(data);
+    } catch {
+      res.status(500).json({ error: "Failed to fetch Zora data." });
+    }
+  }
+  
